9139e9136921be708aa955337d38ca0e1ebfae91
JoelCaquene <joelcaquenejr@gmail.com>
2025-12-07 17:42:07 +0100

    Atualizar arquivo Url.py

--- ddb/urls.py
+++ ddb/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings # Importar settings
+from django.conf.urls.static import static # Importar static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
-# CORREÇÃO: Adiciona a rota de mídia (comprovativos) para ser executada 
-# mesmo quando DEBUG=False.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 💡 CORREÇÃO: As rotas estáticas e de mídia devem ser adicionadas 
+# APENAS se estiver em modo de desenvolvimento (DEBUG=True).
+if settings.DEBUG:
+    # 1. Rotas Estáticas (para imagens do carrossel, etc.)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # 2. Rotas de Mídia (para comprovativos, etc., localmente)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
